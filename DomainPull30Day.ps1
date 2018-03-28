57b3f27b35799cf03fecb1c3d56d9e4b68253865
McCluskey <dmcclusk@cscinfo.com>
2018-03-28 09:42:48 -0400

    first commit

---
+++ DomainPull30Day.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module ActiveDirectory
+$Date = [DateTime]::Today.AddDays(-31)
+
+Get-ADComputer -filter {operatingsystem -like '*server*' -and passwordlastset -gt $Date} -properties * | Select Name, DistinguishedName, OperatingSystem, PasswordLastSet | Export-CSV -Path C:\Temp\Domain_Server_List.csv
